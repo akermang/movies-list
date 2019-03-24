08e9487eeee7940405b5dbd3d6df6a6dbaae552d
galakerman <akermang@gmail.com>
2019-03-24 20:40:35 +0200

    Updates

---
+++ precache-manifest.7cad3ad8ef99c1d8e726c5906a9fc2e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e09d44f95b68f418d653",
+    "url": "/movies-list/static/js/runtime~main.899024de.js"
+  },
+  {
+    "revision": "59d2c5b89afecac51b33",
+    "url": "/movies-list/static/js/main.8ab288d9.chunk.js"
+  },
+  {
+    "revision": "707a6fcc7ae986ac25bd",
+    "url": "/movies-list/static/js/2.4ee1de45.chunk.js"
+  },
+  {
+    "revision": "59d2c5b89afecac51b33",
+    "url": "/movies-list/static/css/main.801cd6ca.chunk.css"
+  },
+  {
+    "revision": "fdf8903a49c176e849b3e47583c804d3",
+    "url": "/movies-list/index.html"
+  }
+];
